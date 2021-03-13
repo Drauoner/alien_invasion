69d8ec5515afb66efe4abb80ab7a88fb329dcf59
Alex <alex_renouard1@naylor.edu>
2021-03-12 18:12:35 -0600

    3_12_commit2

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,29 +27,35 @@ class AlienInvasion:
     def run_game(self):
         # Starts the main loop for the game.
         while True:
-                self._check_events()
-                self._update_screen()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-
-    def _check_events():
-        '''Respond to keypreseses and mouse events'''
-             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        # Move the ship to the right.
-                        self.ship.rect.x +=1
+    def _check_events(self):
+        """Respond to keypreseses and mouse events"""
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
-        '''Update images on the screen, and flip to the new screen.'''
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+        """Update images on the screen, and flip to the new screen."""
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
